ff66f9f8c5a02f43f4dd19ca04dae34de989f0ff
knight <karvin_jimenez@hotmail.com>
2025-01-18 15:29:07 -0400

    improvements to the template.

--- source/starter.h
+++ source/starter.h
@@ -3,6 +3,10 @@
 #include <nds.h>
 #include <gl2d.h>
 
+// some useful defines
+#define HALF_WIDTH (SCREEN_WIDTH / 2)
+#define HALF_HEIGHT (SCREEN_HEIGHT / 2)
+
 typedef struct
 {
 	float x;
@@ -11,6 +15,11 @@ typedef struct
 	float h;
 	unsigned int color;
 } Rectangle;
+
+const int WHITE = RGB15(255, 255, 255);
+const int RED = RGB15(255, 0, 0);
+const int GREEN = RGB15(0, 255, 0);
+const int BLUE = RGB15(0, 0, 255);
 
 void drawRectangle(Rectangle &rectangle);
 
